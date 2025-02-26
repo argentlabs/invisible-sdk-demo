c2d3d8591daac9214b04f5ac0e1fb5080e656d40
Aleksandar Cakalic <aleksandar@argent.xyz>
2025-02-26 17:20:03 +0100

    fix: wrong `paymasterParams` assignment

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -12,7 +12,7 @@ const ARGENT_DUMMY_CONTRACT_ADDRESS =
 	isMainnet ? "0x001c515f991f706039696a54f6f33730e9b0e8cc5d04187b13c2c714401acfd4" : "0x07557a2fbe051e6327ab603c6d1713a91d2cfba5382ac6ca7de884d3278636d7";
 const ARGENT_DUMMY_CONTRACT_ENTRYPOINT = "increase_number";
 
-const paymasterParams = isMainnet ? undefined : {
+const paymasterParams = !process.env.NEXT_PUBLIC_AVNU_PAYMASTER_API_KEY ? undefined : {
 	apiKey: process.env.NEXT_PUBLIC_AVNU_PAYMASTER_API_KEY,
 }
 
